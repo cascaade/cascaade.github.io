279f2d0d2c75eba0597dec3d6beb4f2d432121f3
Owen <189063419+cascaade@users.noreply.github.com>
2026-01-10 23:31:51 -0500

    Add hero background

--- src/components/sections/HomeHero/HomeHero.tsx
+++ src/components/sections/HomeHero/HomeHero.tsx
@@ -35,11 +35,54 @@ export default function HomeHero() {
     };
 
     return (
-        <div className={`${styles.hero} ${isMobile && styles.mobile}`}>
-            <div className={styles.left}>
-                <span className={styles.intro}>Hi, I'm</span>
-                <span className={styles.name}>Owen</span>
-                {isMobile && (
+        <div className={styles.heroWrapper}>
+            <div className={`${styles.hero} ${isMobile && styles.mobile}`}>
+                <div className={styles.left}>
+                    <span className={styles.intro}>Hi, I'm</span>
+                    <span className={styles.name}>Owen</span>
+                    {isMobile && (
+                        <div className={styles.right}>
+                            <div className={styles.attribute}>
+                                <img src={locationIcon} className={styles.icon} />
+                                <span>Located in New England, USA</span>
+                            </div>
+                            <div className={styles.attribute}>
+                                <img src={starIcon} className={styles.icon} />
+                                <span>Built 10+ Projects</span>
+                            </div>
+                            <div className={styles.attribute}>
+                                <img src={codeIcon} className={styles.icon} />
+                                <span>
+                                    <strong>Software Engineer</strong>
+                                </span>
+                            </div>
+                        </div>
+                    )}
+                    <span className={styles.desc}>
+                        Crafting web experiences that users actually enjoy, pixel by
+                        pixel.
+                    </span>
+                    <div className={styles.skillsContainer}>
+                        <span className={styles.skill}>React</span>
+                        <span className={styles.skill}>Vite</span>
+                        <span className={styles.skill}>Node.js</span>
+                    </div>
+                    <div className={styles.buttonsContainer}>
+                        <Button
+                            type="primary"
+                            onButtonClick={() => scrollTo("projects")}
+                        >
+                            Explore Projects
+                        </Button>
+                        <Button
+                            type="secondary"
+                            onButtonClick={() => scrollTo("contact")}
+                        >
+                            Reach Out
+                        </Button>
+                    </div>
+                </div>
+                {!isMobile && (
                     <div className={styles.right}>
                         <div className={styles.attribute}>
                             <img src={locationIcon} className={styles.icon} />
@@ -57,48 +100,7 @@ export default function HomeHero() {
                         </div>
                     </div>
                 )}
-                <span className={styles.desc}>
-                    Crafting web experiences that users actually enjoy, pixel by
-                    pixel.
-                </span>
-                <div className={styles.skillsContainer}>
-                    <span className={styles.skill}>React</span>
-                    <span className={styles.skill}>Vite</span>
-                    <span className={styles.skill}>Node.js</span>
-                </div>
-                <div className={styles.buttonsContainer}>
-                    <Button
-                        type="primary"
-                        onButtonClick={() => scrollTo("projects")}
-                    >
-                        Explore Projects
-                    </Button>
-                    <Button
-                        type="secondary"
-                        onButtonClick={() => scrollTo("contact")}
-                    >
-                        Reach Out
-                    </Button>
-                </div>
             </div>
-            {!isMobile && (
-                <div className={styles.right}>
-                    <div className={styles.attribute}>
-                        <img src={locationIcon} className={styles.icon} />
-                        <span>Located in New England, USA</span>
-                    </div>
-                    <div className={styles.attribute}>
-                        <img src={starIcon} className={styles.icon} />
-                        <span>Built 10+ Projects</span>
-                    </div>
-                    <div className={styles.attribute}>
-                        <img src={codeIcon} className={styles.icon} />
-                        <span>
-                            <strong>Software Engineer</strong>
-                        </span>
-                    </div>
-                </div>
-            )}
         </div>
     );
 }
